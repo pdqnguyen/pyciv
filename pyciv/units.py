619c2bdab13555fc4e13ddb3cc15b6e598f873ec
Philippe Nguyen <philnguyen94@gmail.com>
2019-08-07 14:49:18 -0700

    better handling of unit actions

--- pyciv/units.py
+++ pyciv/units.py
@@ -7,15 +7,15 @@ UNITS = {
         'movement': 2,
         'cost': {
             'gold': 100,
-            'production': 1
+            'production': 50
         }
     },
     'worker': {
         'type': 'worker',
         'movement': 2,
         'cost': {
-            'gold': 100,
-            'production': 1
+            'gold': 50,
+            'production': 30
         }
     }
 }
@@ -27,6 +27,7 @@ def create_unit(name, unit_class, **kwargs):
     kwargs.update(**d)
     cls_ = eval(unit_type[0].upper() + unit_type[1:].lower() + 'Unit')
     return cls_(name, **kwargs)
+
 
 class Unit:
     def __init__(self, name, _class=None, pos=None, civ=None, movement=2, cost=None):
@@ -48,42 +49,52 @@ class Unit:
     def reset_moves(self):
         self.moves = self.movement
 
+    def get_moves(self, game):
+        out = []
+        tiles_in_range = civutils.tiles_in_range(self.pos, 1, game.shape)
+        for x, y in tiles_in_range:
+            tile = game.board[x, y]
+            if self.moves >= tile.moves:
+                target_unit = game.get_unit(tile)
+                target_city = game.get_city(tile)
+                if not target_unit and not target_city:
+                    out.append((x, y))
+                elif target_unit:
+                    if target_unit.civ == self.civ and target_unit._class != self._class:
+                        out.append((x, y))
+                elif target_city:
+                    if target_city.civ == self.civ:
+                        out.append((x, y))
+        return out
 
-class CivilianUnit(Unit):
-    def __init__(self, name, **kwargs):
-        super(CivilianUnit, self).__init__(name, **kwargs)
 
-
-class SettlerUnit(CivilianUnit):
+class SettlerUnit(Unit):
     def __init__(self, name, **kwargs):
         super(SettlerUnit, self).__init__(name, **kwargs)
 
     def actions(self, game):
-        out = (['Move'] if self.moves else [])
-        nearby_tiles = civutils.tiles_in_range(self.pos, 3, game.shape[0])
+        out = (['move'] if self.get_moves(game) else [])
+        nearby_tiles_xy = civutils.tiles_in_range(self.pos, 3, game.shape)
+        nearby_tiles = [game.board[x, y] for x, y in nearby_tiles_xy]
         if not any(game.get_city(tile) for tile in nearby_tiles):
-            out.append('Settle')
+            out.append('settle')
         return out
 
-    def settle(self, game):
-        game.settle(self)
 
-
-class WorkerUnit(CivilianUnit):
+class WorkerUnit(Unit):
     def __init__(self, name, **kwargs):
         super(WorkerUnit, self).__init__(name, **kwargs)
 
     def actions(self, game):
-        out = (['Move'] if self.moves else [])
+        out = (['move'] if self.get_moves(game) else [])
         tile = game.board[self.pos]
         civ = game.get_civ(tile)
         city = game.get_city(tile)
         civ_name = (civ.name if civ else '')
         if self.civ == civ_name and not city:
-            if 'forest' in tile.features or 'rainforest' in tile.features:
+            if ('forest' in tile.features or 'rainforest' in tile.features) and 'lumber mill' not in tile.improvements:
                 out.append('chop')
-            else:
-                out += improvement_options(tile)
+            out += improvement_options(tile)
         return out
 
 
