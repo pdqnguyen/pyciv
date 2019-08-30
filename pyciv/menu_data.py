a2b1eb053c7f082d8d1981dbf85374c78c599a6c
pdqnguyen <philnguyen94@gmail.com>
2019-08-29 23:40:24 -0700

    moved menu-making functions to menu_data

---
+++ pyciv/menu_data.py
@@ -0,0 +1,83 @@
+def get_default_options():
+    options = (
+        'Main',
+        'End turn',
+        'Close menu',
+        'Quit game',
+    )
+    return options
+
+
+def get_city_options(game, city, civ, units=[]):
+    prod_current = "Production - {} ({:.1f}/{})".format(
+        city.prod,
+        city.prod_progress,
+        city.get_item_cost(city.prod, 'production')
+    ) if city.prod else "Production"
+    prod_options = city.prod_options() if city.prod_options() else ["No production options"]
+    buildings = [b.name for b in city.buildings]
+    civ_options = get_civ_options(civ)
+    options = (
+        "{} ({})".format(city.name, city.civ),
+        (
+            prod_current,
+            (
+                "Choose production",
+                *prod_options
+            ),
+            (
+                "Buildings",
+                *buildings
+            )
+        ),
+        civ_options,
+    )
+    for unit in units:
+        options += ((get_unit_options(game, unit, civ),) if unit else ("No unit stationed",))
+    options += (
+        "End turn",
+        "Close menu",
+    )
+    return options
+
+
+def get_unit_options(game, unit, civ=None):
+    options = (
+        "Unit actions: {} ({})".format(unit.name, unit._class),
+    )
+    if unit._class == 'worker':
+        options += (
+            "{} moves, {} builds remaining".format(unit.moves, unit.builds),
+        )
+    else:
+        options += (
+            "{} moves remaining".format(unit.moves),
+        )
+    for action in unit.actions(game):
+        options += (action,)
+    if civ:
+        options += (get_civ_options(civ),)
+    return options
+
+
+def get_multi_unit_options(game, units, civ):
+    options = ("Multiple units",)
+    for unit in units:
+        options += (get_unit_options(game, unit),)
+    options += (get_civ_options(civ),)
+    return options
+
+
+def get_civ_options(civ):
+    yields = ("Yields",)
+    totals = ("Totals",)
+    for k, v in civ.yields.items():
+        yields += ("{}: {:.1f}".format(k, v),)
+    for k, v in civ.totals.items():
+        totals += ("{}: {:.1f}".format(k, v),)
+    options = (
+        "Civilization menu",
+        yields,
+        totals
+    )
+    return options
